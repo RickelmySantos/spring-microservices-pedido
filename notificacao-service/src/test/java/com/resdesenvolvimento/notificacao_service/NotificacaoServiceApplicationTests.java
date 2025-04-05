46fd894ca99ce6201c610f1fe4c9fdd7385eb03c
¨Rickelmy <rickrickelmy97@gmail.com>
2025-04-05 14:37:09 -0300

    feat: estrutura inicial

---
+++ notificacao-service/src/test/java/com/resdesenvolvimento/notificacao_service/NotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resdesenvolvimento.notificacao_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
