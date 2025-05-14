e8640404a6973836f18985a377f3a8fadf250481
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-05-14 10:53:00 -0300

    fet: implementação de resilience4j no modulo de pedido

--- pedido-service/src/main/java/com/rsdesenvolvimento/pedido_service/core/client/UsuarioClient.java
+++ pedido-service/src/main/java/com/rsdesenvolvimento/pedido_service/core/client/UsuarioClient.java
@@ -1,12 +1,23 @@
 package com.rsdesenvolvimento.pedido_service.core.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "usuario-api", path = "/api/usuarios")
+@FeignClient(name = "usuario-api", path = "/api/usuarios", fallback = UsuarioClientFallback.class)
 public interface UsuarioClient {
 
+  @CircuitBreaker(name = "usuario-api", fallbackMethod = "fallbackBuscarUsuarioPorId")
+  @Retry(name = "usuario-api")
   @GetMapping("/{id}")
   UsuarioDto buscarUsuarioPorId(@PathVariable Long id);
+
+  default UsuarioDto fallbackBuscarUsuarioPorId(Long id, Throwable throwable) {
+    UsuarioDto fallback = new UsuarioDto();
+    fallback.setId(id);
+    fallback.setNome("Usuário não encontrado - fallback ativado");
+    return fallback;
+  }
 }
