366789760e14a11a612b77ed93f2939788b7a026
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-05-14 09:53:25 -0300

    wip: configuração temporaria para build

--- usuario/src/main/java/com/rsdesenvolvimento/usuario/modelo/mappers/UsuarioMapper.java
+++ usuario/src/main/java/com/rsdesenvolvimento/usuario/modelo/mappers/UsuarioMapper.java
@@ -5,11 +5,18 @@ import com.rsdesenvolvimento.usuario.modelo.dtos.UsuarioResponseDto;
 import com.rsdesenvolvimento.usuario.modelo.entidades.Usuario;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
+  @Mapping(target = "dataHoraCriacao", ignore = true)
+  @Mapping(target = "criadoPor", ignore = true)
+  @Mapping(target = "dataHoraAtualizacao", ignore = true)
+  @Mapping(target = "atualizadoPor", ignore = true)
+  @Mapping(target = "id", ignore = true)
   Usuario toEntity(UsuarioRequestDto dto);
+
 
   UsuarioResponseDto toDto(Usuario entity);
 
