cd1f7249a5cdf9a088c9214b657e36f2f9d399d8
¨Rickelmy <rickrickelmy97@gmail.com>
2025-05-13 21:53:11 -0300

    wip: erro build

--- notificacao-service/src/main/java/com/rsdesenvolvimento/notificacao_service/controladores/NotificacaoApi.java
+++ notificacao-service/src/main/java/com/rsdesenvolvimento/notificacao_service/controladores/NotificacaoApi.java
@@ -1,27 +1,20 @@
 package com.rsdesenvolvimento.notificacao_service.controladores;
 
-import com.rsdesenvolvimento.notificacao_service.service.NotificacaoService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping("/notificacoes")
+// public class NotificacaoApi {
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/notificacoes")
-public class NotificacaoApi {
-
-  private final NotificacaoService notificacaoService;
+// private final NotificacaoService notificacaoService;
 
 
-  @PostMapping("/enviar-notificacao")
-  public String enviarNotificacao(@RequestBody String email) {
-    try {
-      this.notificacaoService.enviarEmail(email, "Seu pedido foi realizado com sucesso!");
-      return "Notificação enviada!";
-    } catch (Exception e) {
-      return "Erro ao enviar notificação: " + e.getMessage();
-    }
-  }
-}
+// @PostMapping("/enviar-notificacao")
+// public String enviarNotificacao(@RequestBody String email) {
+// try {
+// this.notificacaoService.enviarEmail(email, "Seu pedido foi realizado com sucesso!");
+// return "Notificação enviada!";
+// } catch (Exception e) {
+// return "Erro ao enviar notificação: " + e.getMessage();
+// }
+// }
+// }
--- pedido-service/src/main/java/com/rsdesenvolvimento/pedido_service/core/client/NotificacaoRequestDto.java
+++ pedido-service/src/main/java/com/rsdesenvolvimento/pedido_service/core/client/NotificacaoRequestDto.java
@@ -1,8 +1,12 @@
 package com.rsdesenvolvimento.pedido_service.core.client;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class NotificacaoRequestDto {
 
   private String emailDestino;
