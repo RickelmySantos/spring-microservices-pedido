0de103efe493104c2d0c11ca0db443c10cbdb930
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-05-14 09:53:25 -0300

    wip: configuração temporaria para build

--- pedido-service/src/main/java/com/rsdesenvolvimento/pedido_service/modelo/mappers/PedidoMapper.java
+++ pedido-service/src/main/java/com/rsdesenvolvimento/pedido_service/modelo/mappers/PedidoMapper.java
@@ -4,10 +4,19 @@ import com.rsdesenvolvimento.pedido_service.modelo.dtos.PedidoRequesteDto;
 import com.rsdesenvolvimento.pedido_service.modelo.dtos.PedidoResponseDto;
 import com.rsdesenvolvimento.pedido_service.modelo.entidades.Pedido;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PedidoMapper {
 
+  @Mapping(target = "dataHoraCriacao", ignore = true)
+  @Mapping(target = "criadoPor", ignore = true)
+  @Mapping(target = "dataHoraAtualizacao", ignore = true)
+  @Mapping(target = "atualizadoPor", ignore = true)
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "nomeUsuario", ignore = true)
+  @Mapping(target = "emailUsuario", ignore = true)
+  @Mapping(target = "status", ignore = true)
   Pedido paraEntidade(PedidoRequesteDto dto);
 
   PedidoResponseDto paraDto(Pedido entidade);
