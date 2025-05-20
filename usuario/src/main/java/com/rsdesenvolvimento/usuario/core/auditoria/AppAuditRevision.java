2b03e152c63a153b2ec368c3c14b1ffd1cf7b094
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-05-20 12:14:29 -0300

    wip: implementacao auditoria

---
+++ usuario/src/main/java/com/rsdesenvolvimento/usuario/core/auditoria/AppAuditRevision.java
@@ -0,0 +1,15 @@
+package com.rsdesenvolvimento.usuario.core.auditoria;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import org.hibernate.envers.RevisionEntity;
+
+@Entity
+@Table(name = AuditRevision.TABLE_NAME, schema = AppAuditRevision.APP_AUDIT_SCHEMA_NAME)
+@SequenceGenerator(name = AuditRevision.SEQUENCE_GENERATOR, initialValue = 1, allocationSize = 1,
+    schema = AppAuditRevision.APP_AUDIT_SCHEMA_NAME)
+@RevisionEntity(AppRevisionListener.class)
+public class AppAuditRevision extends AuditRevision {
+  static final String APP_AUDIT_SCHEMA_NAME = "usuario_aud";
+}
---
+++ usuario/src/main/java/com/rsdesenvolvimento/usuario/core/auditoria/AuditRevision.java
@@ -0,0 +1,38 @@
+package com.rsdesenvolvimento.usuario.core.auditoria;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
+
+@FieldNameConstants
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public class AuditRevision {
+
+
+  public static final String SEQUENCE_GENERATOR = "revinfoSeq";
+  public static final String SEQUENCE_NAME = "revinfo_seq";
+  public static final String TABLE_NAME = "revinfo";
+
+  @Id
+  @GeneratedValue(generator = AuditRevision.SEQUENCE_GENERATOR, strategy = GenerationType.SEQUENCE)
+  @RevisionNumber
+  private int id;
+
+  @RevisionTimestamp
+  private long timestamp;
+
+  @Column(name = "usuario")
+  private String usuario;
+
+}
