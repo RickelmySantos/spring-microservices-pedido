f690404df811234c61d2fd754d59ba8816d8e3b5
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-05-26 09:12:09 -0300

    feat: Ajustes Gerais

--- rsdesenvolvimento-ui/src/app/app.component.ts
+++ rsdesenvolvimento-ui/src/app/app.component.ts
@@ -1,18 +1,16 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  template: ` <router-outlet></router-outlet>`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  standalone: true,
-  imports: [RouterOutlet],
+    selector: 'app-root',
+    template: `
+        <router-outlet></router-outlet>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    standalone: true,
+    imports: [RouterOutlet],
 })
 export class AppComponent {
-  title = 'rsdesenvolvimento-ui';
+    title = 'rsdesenvolvimento-ui';
 }
