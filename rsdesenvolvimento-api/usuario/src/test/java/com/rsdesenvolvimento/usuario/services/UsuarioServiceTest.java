91862ccadf5bac7969b71fcab1f76cc15d0ad854
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-06-02 12:19:38 -0300

    wip: configuracao testes

---
+++ rsdesenvolvimento-api/usuario/src/test/java/com/rsdesenvolvimento/usuario/services/UsuarioServiceTest.java
@@ -0,0 +1,31 @@
+package com.rsdesenvolvimento.usuario.services;
+
+import com.rsdesenvolvimento.usuario.modelo.mappers.UsuarioMapper;
+import com.rsdesenvolvimento.usuario.repositorios.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class UsuarioServiceTest {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Mock
+    private UsuarioMapper usuarioMapper;
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+}
