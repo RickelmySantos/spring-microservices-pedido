23f7905bc10ad747cd16b7e286f4586d96125e7d
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-07-07 12:06:20 -0300

    feat: adção badger no carrinho

--- rsdesenvolvimento-ui/src/app/core/layout/menu/menu.component.ts
+++ rsdesenvolvimento-ui/src/app/core/layout/menu/menu.component.ts
@@ -1,12 +1,14 @@
-import { NgFor } from '@angular/common';
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { MenuItem } from 'primeng/api';
 import { SharedModule } from 'src/app/shared/shared.module';
 
-import { CarrinhoComponent } from '../../../shared/components/carrinho/carrinho.component';
-import { MenuItemComponent } from '../menu-item/menu-item.component';
+import { BadgeModule } from 'primeng/badge';
+import { CarrinhoService } from 'src/app/services/carrinho.service';
+import { MenuItemComponent } from 'src/app/core/layout/menu-item/menu-item.component';
+import { CarrinhoComponent } from 'src/app/shared/components/carrinho/carrinho.component';
 
 @Component({
     selector: 'app-menu',
@@ -29,19 +31,23 @@ import { MenuItemComponent } from '../menu-item/menu-item.component';
                 </ul>
             </nav>
             <div>
-                <a (click)="modal.open()" class="cursor-pointer">
+                <a (click)="carrinhoComponent.open()" class="cursor-pointer">
                     <fa-icon [icon]="['fas', 'cart-shopping']" class="text-2xl text-white"></fa-icon>
+                    <p-badge *ngIf="(quantidadeItens$ | async) > 0" [value]="(quantidadeItens$ | async)!" severity="danger" styleClass="absolute -top-2 -right-3"></p-badge>
                 </a>
-                <app-carrinho [carrinhoItem]="items" #modal></app-carrinho>
+                <app-carrinho #carrinhoComponent></app-carrinho>
             </div>
         </section>
     `,
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [SharedModule, NgFor, MenuItemComponent, CarrinhoComponent],
+    imports: [SharedModule, NgIf, NgFor, MenuItemComponent, CarrinhoComponent, BadgeModule],
 })
 export class MenuComponent {
+    private readonly carrinhoService: CarrinhoService = inject(CarrinhoService);
+    readonly quantidadeItens$ = this.carrinhoService.quantidadeItens$;
+
     isCarrinhoVisible = false;
 
     constructor(library: FaIconLibrary) {
@@ -53,11 +59,6 @@ export class MenuComponent {
         { label: 'Cardápio', routerLink: '/menu' },
         { label: 'Sobre', url: '#about' },
         { label: 'Contato', url: '#contact' },
-    ];
-
-    items = [
-        { name: 'Feijoada', quantity: 1 },
-        { name: 'Pão de Queijo', quantity: 3 },
     ];
 
     openCarrinho(): void {
